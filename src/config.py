1f2684ffa8b9a2ee24a580ed9a0b60b8f0a0d7bf
Bence Markiel <105103590+bceenaeiklmr@users.noreply.github.com>
2026-05-31 18:02:21 +0200

    Initial configuration file commit

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# Output folder
+OUTPUT_PATH = "D:/jobs/profession"
+
+# Profiles
+PROFILES = {
+  "marketing": "https://www.profession.hu/allasok/online-marketing/{},12,0,0,219",
+  "crm": "https://www.profession.hu/allasok/budapest/{},0,23,crm%20elemz%c5%91,0,0,67,0,0,0,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,1",
+  "marketing_hybrid": "https://www.profession.hu/allasok/marketing-media-pr/budapest/{},12,23,0,0,0,0,0,0,0,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5",
+  "erp": "https://www.profession.hu/allasok/{},0,0,erp,0,0,0,0,0,0,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5",
+}
